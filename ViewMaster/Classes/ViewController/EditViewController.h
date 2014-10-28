daabc91a60350ac290fadc08915fadac3b65bc70
HamanyanDev <tvt@TVT-01-316.local>
2014-10-28 14:00:43 +0900

    1028
    test

---
+++ ViewMaster/Classes/ViewController/EditViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditViewController.h
+//  ViewMaster
+//
+//  Created by tvt on 2014/10/03.
+//  Copyright (c) 2014年 HiroyukiHamana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController :  UITableViewController <UITableViewDataSource,UITableViewDelegate>
+
+@end
